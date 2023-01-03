ff9efa51f271fff09b8b809486da08055e3c50c9
Harshitha P <65996712+puffy101@users.noreply.github.com>
2023-01-03 15:19:06 +0530

    Updated API

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const product_list = require("./product_list.js");
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-    origin: 'https://laundry-frontend-1bzk.onrender.com',
+    origin: 'https://laundry-frontend-ggle.onrender.com',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
